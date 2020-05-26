3f2c468f95b4f2e5f441427f3d9ed3fb105d41b1
Ning Wang <wangning2nwpu@gmail.com>
2020-05-26 19:21:35 +0200

    small update

--- Bayesian/util.py
+++ Bayesian/util.py
@@ -2,8 +2,25 @@ import numpy as np
 import itertools
 
 
-def generate_binary_coef(max_non_zeros=1, length=10):
+def generate_binary_coef_random(length, n_samples=10):
     """
+    Generate a list of binary coefficient vectors randomly
+    length.............. int, length of the binary coefficients
+    n_samples........... int, number of vectors
+
+    return: 2D ndarray with each row a  binary coefficient vector.
+            shape: (n_samples, length)
+            dtype: np.int32
+    """
+    return np.random.randint(low=0,high=2,size=(n_samples, length),dtype=np.int32) 
+
+
+
+def generate_binary_coef_simple(length, max_non_zeros=1):
+    """
+    Generate all possible binary coefficient vectors with an user-specified 
+    maximum number of non-zero entries.
+    
     max_non_zeros........non-negative int, maximum non-zero values
     length...............int, length of the binary coefficients
 
